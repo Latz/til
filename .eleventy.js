05df3b7b15fcb3307a08f81ede609ade9bc0ebd9
Latz <latz@elektroelch.de>
2021-10-19 21:15:29 +0200

    Added Syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
 const moment = require("moment");
 const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 moment.locale("en");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("dateIso", (date) => {
+  eleventyConfig.addFilter("dateIso", date => {
     return moment(date).toISOString();
   });
 
-  eleventyConfig.addFilter("dateReadable", (date) => {
+  eleventyConfig.addFilter("dateReadable", date => {
     return moment(date).format("LL"); // E.g. May 31, 2019
   });
   eleventyConfig.addPassthroughCopy("css");
@@ -17,6 +18,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addPlugin(pluginRss);
 };
--- posts/blog-post-157.md
+++ posts/blog-post-157.md
@@ -4,4 +4,4 @@ date: 2021-10-19
 tags: ["post"]
 ---
 
-... that I can replace "return new Promise((resolve) => resolve(true)));" with "return Promise.resolve(true);". #javascript
+... that I can replace `return new Promise((resolve) => resolve(true)));` with `return Promise.resolve(true);`. #javascript
